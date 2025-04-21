3048af014053b1eff0227f48f9b42cb4ce4e74f7
shakana0 <shakanajamal3@gmail.com>
2025-04-21 17:09:48 +0200

    added fotter and content

--- data/index.ts
+++ data/index.ts
@@ -181,14 +181,14 @@ export const workExperience = [
         id: 2,
         title: "Mobile App Dev - JSM Tech",
         desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-        className: "md:col-span-2", // change to md:col-span-2
+        className: "md:col-span-2",
         thumbnail: "/exp2.svg",
     },
     {
         id: 3,
         title: "Freelance App Dev Project",
         desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-        className: "md:col-span-2", // change to md:col-span-2
+        className: "md:col-span-2",
         thumbnail: "/exp3.svg",
     },
     {
@@ -204,13 +204,11 @@ export const socialMedia = [
     {
         id: 1,
         img: "/git.svg",
-    },
-    {
-        id: 2,
-        img: "/twit.svg",
+        link: "https://github.com/shakana0",
     },
     {
         id: 3,
         img: "/link.svg",
+        link: "https://linkedin.com/in/shakana-jamal"
     },
 ];
