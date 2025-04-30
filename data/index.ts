a3ec06c7efdee19c3e8f39d00874cafb1a7a46bd
shakana0 <shakanajamal3@gmail.com>
2025-04-30 11:28:06 +0200

    removed unused images and updated data

--- data/index.ts
+++ data/index.ts
@@ -79,28 +79,20 @@ export const projects = [
     },
     {
         id: 2,
-        title: "Yoom - Video Conferencing App",
-        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-        img: "/p2.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "/ui.yoom.com",
+        title: "Café Mundo",
+        des: "is a full-stack application where you can browse and buy different coffee products.",
+        img: "/coffee-app-home.png",
+        iconLists: ["/re.svg", "/ts.svg", "styledcomponents.svg", "/node-js.svg", "/mdb.svg"],
+        link: "https://github.com/shakana0/Recept-app",
     },
     {
         id: 3,
-        title: "AI Image SaaS - Canva Application",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/p3.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    },
-    {
-        id: 4,
-        title: "Animated Apple Iphone 3D Website",
-        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/p4.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "/ui.apple.com",
-    },
+        title: "Backend-focused API",
+        des: "Backend-first e-commerce API with CQRS, MediatR, and EF Core.",
+        img: "/backend-diagram.jpg",
+        iconLists: ["/csharp.svg", "/dotnet.svg", "/sql.svg"],
+        link: "https://github.com/shakana0/BakeryStoreBackend",
+    }
 ];
 
 export const workExperience = [
