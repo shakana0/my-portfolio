86fa6d2f862240e4e384a8d50dd2cd4d29f7b850
shakana0 <shakanajamal3@gmail.com>
2026-04-01 19:05:58 +0200

    style: update card styling

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,39 +7,38 @@ import ImageSlider from "./ImageSlider";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20" id="projects">
-      <h1 className="heading">
+    <div className="py-20 w-full" id="projects">
+      <h1 className="heading mb-10 text-center">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-8 xl:gap-20">
+
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-x-12 gap-y-20 md:gap-y-28 xl:gap-y-32 px-4 max-w-7xl mx-auto justify-items-center">
         {projects.map((item) => (
           <div
-            className="md:pt-10 xl:pt-20 2xl:pt-40 sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
             key={item.id}
+            className="flex items-center justify-center sm:w-[570px] w-[90vw] min-h-[30rem] sm:min-h-[35rem] relative"
           >
             <PinContainer title={item.title} href={item.link}>
-              <a href={item.link} target="_blank" rel="noopener noreferrer">
-                <div className="relative flex items-center justify-center w-[80vw] sm:w-[570px] max-w-[1440px] overflow-hidden h-[30vh] sm:h-[40vh] xl:h-[40vh] 2xl:h-[38vh] max-h-[600px] aspect-[16/9] mb-10">
-                  <img
-                    src="/bg.png"
-                    alt="bgimg"
-                    className="absolute inset-0 z-0 w-full h-full object-cover lg:rounded-3xl"
-                  />
-
+              <div className="flex flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] sm:w-[570px] h-full">
+                <div className="relative flex items-center justify-center w-full overflow-hidden aspect-[16/9] mb-6 rounded-xl">
+                  <div className="absolute inset-0 z-0 w-full h-full bg-[#13162D] overflow-hidden">
+                    <img
+                      src="/bg.png"
+                      alt="bgimg"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
                   <ImageSlider images={item.img} />
                 </div>
 
-                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1 text-white">
                   {item.title}
                 </h1>
 
                 <p
-                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                  style={{
-                    color: "#BEC1DD",
-                    margin: "1vh 0",
-                  }}
+                  className="lg:text-lg lg:font-normal font-light text-sm line-clamp-2 mt-2"
+                  style={{ color: "#BEC1DD" }}
                 >
                   {item.des}
                 </p>
@@ -54,18 +53,19 @@ const RecentProjects = () => {
                           transform: `translateX(-${5 * index + 2}px)`,
                         }}
                       >
-                        <img src={icon} alt="icon5" className="p-2" />
+                        <img src={icon} alt="icon" className="p-2" />
                       </div>
                     ))}
                   </div>
-                  <span className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                      View On GitHub
+
+                  <div className="flex justify-center items-center">
+                    <p className="flex lg:text-lg md:text-xs text-sm text-purple">
+                      Check Live Site
                     </p>
                     <FaLocationArrow className="ms-3" color="#CBACF9" />
-                  </span>
+                  </div>
                 </div>
-              </a>
+              </div>
             </PinContainer>
           </div>
         ))}
